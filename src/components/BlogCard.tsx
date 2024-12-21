35c20f605fb6551f45dba5ae711c0b2f27d970f3
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2024-12-21 23:47:03 +0500

    added eight commit

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from "react";
 import { Card, CardContent, CardTitle } from "../components/ui/Card";
-import Image from "next/image";
+
 
 interface BlogCardProps {
   post: {
